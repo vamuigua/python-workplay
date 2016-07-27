8f65bb5233901b12813b90cc432e714cdf360da0
Victor Allen <vamuigua@gmail.com>
2016-07-27 16:10:57 +0300

    modified py

--- verification/CODE.py
+++ verification/CODE.py
@@ -1,9 +1,15 @@
+#Prompt window to write your name
 name = raw_input("Please write your name...")
+#Open file of names.txt
 openfile = open("names.txt", "r+")
+#Check if name is in file
 if name not in openfile.readline():
+#write name in file if note found
     openfile.write(name)
+#file closes
     openfile.close()
     print ("Success!")
+#Else statement to print if false
 else:
     print ('Name not found')
     openfile.close()
